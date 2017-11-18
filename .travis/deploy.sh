188af09e09bdbfa3fb614c97923029aff81c80a8
sueka <mail@sueka.me>
2017-11-18 18:01:54 +0900

    :unlock: cease to set permissions

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,5 +1,5 @@
 #!bin/sh
 
-find $TRAVIS_BUILD_DIR/_site -type f -print | xargs chmod 604
-find $TRAVIS_BUILD_DIR/_site -type d -print | xargs chmod 701
+# find $TRAVIS_BUILD_DIR/_site -type f -print | xargs chmod 604
+# find $TRAVIS_BUILD_DIR/_site -type d -print | xargs chmod 701
 rsync --quiet -rpt --delete-after $TRAVIS_BUILD_DIR/_site/ http@sueka.me:/srv/http/www/html
