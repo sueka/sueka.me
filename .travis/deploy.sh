8c6a0d52b70ffee76cbacc482405efa76ff34a04
sueka <mail@sueka.me>
2017-11-18 18:35:36 +0900

    :grey_question: make experiment for CI

--- .travis/deploy.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# find $TRAVIS_BUILD_DIR/_site -type f -print | xargs chmod 604
-# find $TRAVIS_BUILD_DIR/_site -type d -print | xargs chmod 701
-rsync -r --delete-after $TRAVIS_BUILD_DIR/_site/ http@sueka.me:/srv/http/www/html
