5d4c8323086776b476085221e437c9750a3d1e6b
sueka <mail@sueka.me>
2018-01-20 22:24:28 +0900

    :art: remove the frozen string comments

--- Gemfile
+++ Gemfile
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 source 'https://rubygems.org'
 
 gem 'jekyll', '>= 3.6.2', '~> 3.6'
--- spec/browser/home_spec.rb
+++ spec/browser/home_spec.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 RSpec.describe 'sueka.me' do
   describe 'home page' do
     feature 'multilanguage' do
