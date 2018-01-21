3afcf17bd3b055828f168446cd831b5fe029f50b
sueka <mail@sueka.me>
2018-01-21 17:15:11 +0900

    :sparkles: :hammer: support sectioning contents & refactor

--- source/assets/scripts/header-anchor-generator.js
+++ source/assets/scripts/header-anchor-generator.js
@@ -16,18 +16,38 @@
     }
   });
 
-  var hns = window.document.querySelectorAll("main").asArray.map(function (main) {
-    return main.querySelectorAll("h1, h2, h3, h4, h5, h6").asArray;
-  }).flatten.filter(function (hn) {
-    return hn.hasAttribute("id");
-  });
+  Array.prototype.flatMap = function (f) {
+    return this.map(f).flatten;
+  };
 
-  hns.forEach(function (hn) {
+  var mains = window.document.querySelectorAll("main").asArray;
+
+  var anchorageAndIds = mains.flatMap(function (main) {
+    return main.querySelectorAll("h1, h2, h3, h4, h5, h6").asArray;
+  }).filter(function (hn) {
+    return hn.hasAttribute("id");
+  }).map(function (hn) {
+    return {
+      "anchorage": hn,
+      "id": hn.id,
+    };
+  }).concat(mains.flatMap(function (main) {
+    return main.querySelectorAll("section, article, nav, aside").asArray;
+  }).filter(function (section) {
+    return section.hasAttribute("id");
+  }).map(function (section) {
+    return {
+      "anchorage": section.querySelector("h1, h2, h3, h4, h5, h6"),
+      "id": section.id,
+    };
+  }));
+
+  anchorageAndIds.forEach(function (anchorageAndId) {
     var a = window.document.createElement("a");
         a.className = "header-anchor";
-        a.href = "#" + hn.id;
+        a.href = "#" + anchorageAndId.id;
         a.textContent = "🔗";
-    hn.appendChild(a);
+    anchorageAndId.anchorage.appendChild(a);
   });
 
   window.addEventListener("touchstart", function () {
