cb1218316f23f0504cbb2bf3ed6b8dd22d02f2a7
sueka <mail@sueka.me>
2018-01-29 21:50:08 +0900

    :white_check_mark: add tests

--- spec/browser/home_spec.rb
+++ spec/browser/home_spec.rb
@@ -3,11 +3,18 @@ RSpec.describe 'sueka.me' do
     feature 'multilanguage' do
       context 'with English' do
         before { visit '/index.html' }
+        it { expect(page).to have_link('עברית', href: '/index-he.html') }
         it { expect(page).to have_link('日本語', href: '/index-ja.html') }
       end
       context 'with Japanese' do
         before { visit '/index-ja.html' }
         it { expect(page).to have_link('English', href: '/') }
+        it { expect(page).to have_link('עברית', href: '/index-he.html') }
+      end
+      context 'with Hebrew' do
+        before { visit '/index-he.html' }
+        it { expect(page).to have_link('English', href: '/') }
+        it { expect(page).to have_link('日本語', href: '/index-ja.html') }
       end
     end
   end
