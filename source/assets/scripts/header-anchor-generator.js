72a51edd4b7fba77169852881b33c82f0fc6b604
sueka <mail@sueka.me>
2018-02-12 15:27:11 +0900

    :art: remove the wrappers

--- source/assets/scripts/header-anchor-generator.js
+++ source/assets/scripts/header-anchor-generator.js
@@ -1,46 +1,42 @@
-(function (window) {
+function array(arrayLike) {
+  return Array.prototype.slice.call(arrayLike);
+}
 
-  function array(arrayLike) {
-    return Array.prototype.slice.call(arrayLike);
-  }
+function flatten(xss) {
+  return Array.prototype.concat.apply([], xss);
+}
 
-  function flatten(xss) {
-    return Array.prototype.concat.apply([], xss);
-  }
+const mains = array(document.querySelectorAll("main"));
 
-  const mains = array(window.document.querySelectorAll("main"));
+const anchorageAndIds = flatten(mains.map(function (main) {
+  return array(main.querySelectorAll("h1, h2, h3, h4, h5, h6"));
+})).filter(function (hn) {
+  return hn.hasAttribute("id");
+}).map(function (hn) {
+  return {
+    "anchorage": hn,
+    "id": hn.id,
+  };
+}).concat(flatten(mains.map(function (main) {
+  return array(main.querySelectorAll("section, article, nav, aside"));
+})).filter(function (section) {
+  return section.hasAttribute("id");
+}).map(function (section) {
+  return {
+    "anchorage": section.querySelector("h1, h2, h3, h4, h5, h6"),
+    "id": section.id,
+  };
+}));
 
-  const anchorageAndIds = flatten(mains.map(function (main) {
-    return array(main.querySelectorAll("h1, h2, h3, h4, h5, h6"));
-  })).filter(function (hn) {
-    return hn.hasAttribute("id");
-  }).map(function (hn) {
-    return {
-      "anchorage": hn,
-      "id": hn.id,
-    };
-  }).concat(flatten(mains.map(function (main) {
-    return array(main.querySelectorAll("section, article, nav, aside"));
-  })).filter(function (section) {
-    return section.hasAttribute("id");
-  }).map(function (section) {
-    return {
-      "anchorage": section.querySelector("h1, h2, h3, h4, h5, h6"),
-      "id": section.id,
-    };
-  }));
+anchorageAndIds.forEach(function (anchorageAndId) {
+  const space = document.createTextNode(" ");
+  const a = document.createElement("a");
+        a.className = "header-anchor";
+        a.href = "#" + anchorageAndId.id;
+        a.textContent = "🔗";
+  anchorageAndId.anchorage.appendChild(space);
+  anchorageAndId.anchorage.appendChild(a);
+});
 
-  anchorageAndIds.forEach(function (anchorageAndId) {
-    const space = window.document.createTextNode(" ");
-    const a = window.document.createElement("a");
-          a.className = "header-anchor";
-          a.href = "#" + anchorageAndId.id;
-          a.textContent = "🔗";
-    anchorageAndId.anchorage.appendChild(space);
-    anchorageAndId.anchorage.appendChild(a);
-  });
-
-  window.addEventListener("touchstart", function () {
-  }, false);
-
-})(new Function("return this")());
+window.addEventListener("touchstart", function () {
+}, false);
