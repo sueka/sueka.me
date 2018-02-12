a36b2e66877b0d549d88c5322062d2ef470bdfd6
sueka <mail@sueka.me>
2018-02-12 15:34:31 +0900

    :bug: use the directive instead of module type tag for Edge 15 & Firefox

--- source/assets/scripts/header-anchor-generator.js
+++ source/assets/scripts/header-anchor-generator.js
@@ -1,42 +1,48 @@
-function array(arrayLike) {
-  return Array.prototype.slice.call(arrayLike);
-}
+(function () {
 
-function flatten(xss) {
-  return Array.prototype.concat.apply([], xss);
-}
+  "use strict";
 
-const mains = array(document.querySelectorAll("main"));
+  function array(arrayLike) {
+    return Array.prototype.slice.call(arrayLike);
+  }
 
-const anchorageAndIds = flatten(mains.map(function (main) {
-  return array(main.querySelectorAll("h1, h2, h3, h4, h5, h6"));
-})).filter(function (hn) {
-  return hn.hasAttribute("id");
-}).map(function (hn) {
-  return {
-    "anchorage": hn,
-    "id": hn.id,
-  };
-}).concat(flatten(mains.map(function (main) {
-  return array(main.querySelectorAll("section, article, nav, aside"));
-})).filter(function (section) {
-  return section.hasAttribute("id");
-}).map(function (section) {
-  return {
-    "anchorage": section.querySelector("h1, h2, h3, h4, h5, h6"),
-    "id": section.id,
-  };
-}));
+  function flatten(xss) {
+    return Array.prototype.concat.apply([], xss);
+  }
 
-anchorageAndIds.forEach(function (anchorageAndId) {
-  const space = document.createTextNode(" ");
-  const a = document.createElement("a");
-        a.className = "header-anchor";
-        a.href = "#" + anchorageAndId.id;
-        a.textContent = "🔗";
-  anchorageAndId.anchorage.appendChild(space);
-  anchorageAndId.anchorage.appendChild(a);
-});
+  const mains = array(document.querySelectorAll("main"));
 
-window.addEventListener("touchstart", function () {
-}, false);
+  const anchorageAndIds = flatten(mains.map(function (main) {
+    return array(main.querySelectorAll("h1, h2, h3, h4, h5, h6"));
+  })).filter(function (hn) {
+    return hn.hasAttribute("id");
+  }).map(function (hn) {
+    return {
+      "anchorage": hn,
+      "id": hn.id,
+    };
+  }).concat(flatten(mains.map(function (main) {
+    return array(main.querySelectorAll("section, article, nav, aside"));
+  })).filter(function (section) {
+    return section.hasAttribute("id");
+  }).map(function (section) {
+    return {
+      "anchorage": section.querySelector("h1, h2, h3, h4, h5, h6"),
+      "id": section.id,
+    };
+  }));
+
+  anchorageAndIds.forEach(function (anchorageAndId) {
+    const space = document.createTextNode(" ");
+    const a = document.createElement("a");
+    a.className = "header-anchor";
+    a.href = "#" + anchorageAndId.id;
+    a.textContent = "🔗";
+    anchorageAndId.anchorage.appendChild(space);
+    anchorageAndId.anchorage.appendChild(a);
+  });
+
+  window.addEventListener("touchstart", function () {
+  }, false);
+
+})();
--- source/assets/scripts/search.js
+++ source/assets/scripts/search.js
@@ -1,45 +1,51 @@
-function array(arrayLike) {
-  return Array.prototype.slice.call(arrayLike);
-}
+(function () {
 
-function search(patterns, posts) {
-  return posts.filter(post => patterns.every(pattern => pattern.test(post.content)));
-}
+  "use strict";
 
-const qFields = array(document.querySelectorAll("input[name=q]"));
+  function array(arrayLike) {
+    return Array.prototype.slice.call(arrayLike);
+  }
 
-fetch("/json/posts.json").then(response => response.json()).then(posts => {
-  window.addEventListener("keyup", (event) => {
-    if (qFields.includes(event.target)) {
-      const q = event.target.value;
+  function search(patterns, posts) {
+    return posts.filter(post => patterns.every(pattern => pattern.test(post.content)));
+  }
 
-      const resultDiv = event.target.parentNode.querySelector(".search-result");
-      resultDiv.innerHTML = "";
+  const qFields = array(document.querySelectorAll("input[name=q]"));
 
-      if (q !== "") {
-        const searchPatterns = q.split(" ").map(query => new RegExp(query, "i"));
-        // const exceprtPattern = new RegExp("(?:[^>](?!<))+" + q.replace(" ", "|") + "[^<]+(?:</)", "i");
+  fetch("/json/posts.json").then(response => response.json()).then(posts => {
+    window.addEventListener("keyup", (event) => {
+      if (qFields.includes(event.target)) {
+        const q = event.target.value;
 
-        const ul = document.createElement("ul");
-        const liContainer = document.createDocumentFragment();
+        const resultDiv = event.target.parentNode.querySelector(".search-result");
+        resultDiv.innerHTML = "";
 
-        search(searchPatterns, posts).forEach(post => {
-          const li = document.createElement("li");
-          const a = document.createElement("a");
-                a.href = post.url;
-                a.lang = post.lang;
-                a.hreflang = post.lang;
-                a.textContent = post.title;
-          // const div = document.createElement("div");
-          //     div.innerHTML = exceprtPattern.exec(post.content)[0];
+        if (q !== "") {
+          const searchPatterns = q.split(" ").map(query => new RegExp(query, "i"));
+          // const exceprtPattern = new RegExp("(?:[^>](?!<))+" + q.replace(" ", "|") + "[^<]+(?:</)", "i");
 
-          li.appendChild(a);
-          liContainer.appendChild(li);
-        });
+          const ul = document.createElement("ul");
+          const liContainer = document.createDocumentFragment();
 
-        ul.appendChild(liContainer);
-        resultDiv.appendChild(ul);
+          search(searchPatterns, posts).forEach(post => {
+            const li = document.createElement("li");
+            const a = document.createElement("a");
+                  a.href = post.url;
+                  a.lang = post.lang;
+                  a.hreflang = post.lang;
+                  a.textContent = post.title;
+            // const div = document.createElement("div");
+            //     div.innerHTML = exceprtPattern.exec(post.content)[0];
+
+            li.appendChild(a);
+            liContainer.appendChild(li);
+          });
+
+          ul.appendChild(liContainer);
+          resultDiv.appendChild(ul);
+        }
       }
-    }
+    });
   });
-});
+
+})();
