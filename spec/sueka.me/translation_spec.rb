925454c4aa3836649ade8d32da346aa25c0b8479
sueka <mail@sueka.me>
2018-03-03 23:52:52 +0900

    :white_check_mark: add tests to detect missing translations

---
+++ spec/sueka.me/translation_spec.rb
@@ -0,0 +1,23 @@
+require 'yaml'
+
+RSpec.describe 'sueka.me' do
+  describe 'translation data' do
+    let(:translations) { YAML.load_file('source/_data/translations.yml') }
+    it do
+      expect(translations).to all match({
+        'langcode'      => anything,
+        'name'          => anything,
+        'languages'     => anything,
+        'tags'          => anything,
+        'archives'      => anything,
+        'search'        => anything,
+        'contents'      => anything,
+        'recent-posts'  => anything,
+        'related-posts' => anything,
+        'date-format'   => anything,
+      })
+    end
+    it { expect(translations).to include include 'langcode' => 'en' }
+    it { expect(translations).to include include 'langcode' => 'ja' }
+  end
+end
