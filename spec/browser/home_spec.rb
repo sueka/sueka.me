1c882da8d449d946bc8b03f380015b6dd5a58706
sueka <mail@sueka.me>
2018-06-06 21:52:22 +0900

    :hammer: rename the feature

--- spec/browser/home_spec.rb
+++ spec/browser/home_spec.rb
@@ -1,6 +1,6 @@
 RSpec.describe 'sueka.me' do
   describe 'home page' do
-    feature 'multilanguage' do
+    feature 'multilingual' do
       context 'with English' do
         before { visit '/index.html' }
         it { expect(page).to have_link('日本語', href: '/index.ja.html') }
