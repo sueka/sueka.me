6dd83252d25332a2045dda0eca80598bfbf87581
sueka <mail@sueka.me>
2018-07-11 22:08:52 +0900

    :art: use subject & should

--- spec/browser/home_spec.rb
+++ spec/browser/home_spec.rb
@@ -5,14 +5,16 @@ Capybara.app = Rack::Jekyll.new
 
 RSpec.describe 'sueka.me' do
   describe 'home page' do
+    subject { page }
+
     feature 'multilingual' do
       context 'with English' do
         before { visit '/index.html' }
-        it { expect(page).to have_link('日本語', href: '/index.ja.html', exact: true) }
+        it { should have_link('日本語', href: '/index.ja.html', exact: true) }
       end
       context 'with Japanese' do
         before { visit '/index.ja.html' }
-        it { expect(page).to have_link('English', href: '/', exact: true) }
+        it { should have_link('English', href: '/', exact: true) }
       end
     end
   end
