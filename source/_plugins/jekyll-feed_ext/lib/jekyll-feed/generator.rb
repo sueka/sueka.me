8134444deaff885a7873467eaf83ed51c55adefc
sueka <mail@sueka.me>
2018-09-30 12:50:58 +0900

    :wrench: omit content element & update summary format cf. RFC 4287 4.1.2

---
+++ source/_plugins/jekyll-feed_ext/lib/jekyll-feed/generator.rb
@@ -0,0 +1,9 @@
+# The original source (https://github.com/jekyll/jekyll-feed/blob/v0.11.0/lib/jekyll-feed/generator.rb) is provided under the MIT License (https://github.com/jekyll/jekyll-feed/blob/v0.11.0/LICENSE.txt).
+#
+# Original Copyright (c) 2015-present Ben Balter and jekyll-feed contributors
+
+class JekyllFeed::Generator
+  private def feed_source_path
+    @feed_source_path ||= File.expand_path "feed.xml", __dir__
+  end
+end
