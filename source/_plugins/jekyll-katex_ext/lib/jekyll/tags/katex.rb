b69a78e2a662dcabb7c3cc262af8ac031ccfba30
sueka <mail@sueka.me>
2020-11-01 19:43:14 +0900

    :white_check_mark: Pass checks

--- source/_plugins/jekyll-katex_ext/lib/jekyll/tags/katex.rb
+++ source/_plugins/jekyll-katex_ext/lib/jekyll/tags/katex.rb
@@ -1,3 +1,5 @@
+# -*- frozen_string_literal: true -*-
+
 # The original source
 # (https://github.com/linjer/jekyll-katex/blob/v1.0.0/lib/jekyll/tags/katex.rb) is provided
 # under the MIT License (https://github.com/linjer/jekyll-katex/blob/v1.0.0/LICENSE.txt).
@@ -6,18 +8,29 @@
 
 module Jekyll
   module Tags
+    # :nodoc:
     class Katex
       LOG_TOPIC = 'Katex Block:'
       KATEX ||= Jekyll::Katex::KATEX_JS
 
       def render(context)
         latex_source = super
-        rendering_options = Jekyll::Katex::Configuration.global_rendering_options.merge(displayMode: @display)
+        rendering_options = Jekyll::Katex::Configuration.global_rendering_options.merge(
+          displayMode: @display
+        )
 
         if rendering_options[:displayMode]
-          "<div class=\"katex-container\">#{ KATEX.call('katex.renderToString', latex_source, rendering_options) }</div>"
+          <<-EOS
+            <div class="katex-container">
+              #{KATEX.call('katex.renderToString', latex_source, rendering_options)}
+            </div>
+          EOS
         else
-          "<span class=\"katex-wrapper\">#{ KATEX.call('katex.renderToString', latex_source, rendering_options) }</span>"
+          <<-EOS
+            <span class="katex-wrapper">
+              #{KATEX.call('katex.renderToString', latex_source, rendering_options)}
+            </span>
+          EOS
         end
       end
     end
