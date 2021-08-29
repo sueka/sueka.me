4812e59cd40cde0a8319c18f9f172b0d4c935a14
sueka <mail@sueka.me>
2021-08-30 01:34:58 +0900

    :sparkles: Provide header anchors

--- _config.js
+++ _config.js
@@ -2,12 +2,21 @@ import lume from 'lume/mod.ts'
 import date from 'lume/plugins/date.ts'
 import postcss from 'lume/plugins/postcss.ts'
 import relativeUrls from 'lume/plugins/relative_urls.ts'
+import anchor from 'https://jspm.dev/markdown-it-anchor'
+import deflist from 'https://jspm.dev/markdown-it-deflist'
 import postcssPresetEnv from 'https://jspm.dev/postcss-preset-env'
 
 const site = lume({
   src: 'src',
   location: new URL('https://sueka.me'), // TODO: Use {{ site.data.url }}
   prettyUrls: false,
+}, {
+  markdown: {
+    plugins: [
+      [anchor, { level: 2, permalink: anchor.permalink.headerLink() }],
+      deflist,
+    ],
+  },
 })
 .use(date())
 .use(postcss({
