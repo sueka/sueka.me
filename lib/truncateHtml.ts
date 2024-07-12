e44614ef2cc07e029415e9227c9d79d71e6a82e3
sueka <mail@sueka.me>
2024-07-13 00:43:55 +0900

    :alien: Change the library to truncate HTML

--- lib/truncateHtml.ts
+++ lib/truncateHtml.ts
@@ -1,5 +1,6 @@
-import truncate from 'html-truncator'
+import truncate from 'truncate-html'
 
 export default function truncateHtml(safeContent: string, length: number) {
+  // @ts-expect-error: TS2349; truncate-html default-exports truncate().
   return truncate(safeContent, length)
 }
